16e49e85bbd6c44309c9a86892af58a135639012
AliciaAMT <46018401+AliciaAMT@users.noreply.github.com>
2022-11-01 11:13:12 -0500

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'mandalan-tales',
+    appId: '1:714965728567:web:247df5560ab7a819d9aeb2',
+    storageBucket: 'mandalan-tales.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBDgp4nioS2O0SyrBFr8rEECzkN4pLMKSA',
+    authDomain: 'mandalan-tales.firebaseapp.com',
+    messagingSenderId: '714965728567',
+    measurementId: 'G-BDT203TQ5Q',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'mandalan-tales',
+    appId: '1:714965728567:web:247df5560ab7a819d9aeb2',
+    storageBucket: 'mandalan-tales.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBDgp4nioS2O0SyrBFr8rEECzkN4pLMKSA',
+    authDomain: 'mandalan-tales.firebaseapp.com',
+    messagingSenderId: '714965728567',
+    measurementId: 'G-BDT203TQ5Q',
+  },
   production: false
 };
 
